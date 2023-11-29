b28f665bc2b30d9ec1f2bfd54d88b9d345c0582d
Chloé <chloe.du.16@live.fr>
2023-11-29 16:37:29 +0100

    add if and app.put

--- App.js
+++ App.js
@@ -1,34 +1,21 @@
 const express = require('express')
-//On construit une instance d'express 
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-const mockCoworkings = require('./mock-coworking')
+const mockCoworkings = require('./mock-coworkings')
 
-const arrUsers = [
-    {
-        id: 12,
-        name: "Paul",
-        age: 35
-    },
-    {
-        id: 15,
-        name: "Pierre",
-        age: 28
-    }, {
-        id: 6,
-        name: "Mathilde",
-        age: 19
-    }
-]
+// middleware qui me permet d'interpréter le corps de ma requête (req.body) en format json
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World !')
+    res.json('Hello World !')
 })
 
 app.get('/api/coworkings', (req, res) => {
     // Afficher la phrase : Il y a ... coworkings dans la liste. 
-    res.send(`Il y a ${mockCoworkings.length} coworkings dans la liste.`)
+    res.json(mockCoworkings)
 })
 
 app.get('/api/coworkings/:id', (req, res) => {
@@ -37,57 +24,37 @@ app.get('/api/coworkings/:id', (req, res) => {
     if (!result) {
         result = `Aucun élément ne correspond à l'id n°${req.params.id}`
     }
-    res.send(result)
+    res.json(result)
 })
 
-app.checkout('/api/coworkings', (req, res) => {
-  // Afficher une phrase : il y a .... coworkings dans la liste.
-  res.send(`Il y a ${mockCoworkings.length} coworkings dans la liste.`)
+// implémenter le endpoint post qui renvoie une réposne "post fonctionne"
+app.post('/api/coworkings/', (req, res) => {
+    // Ajouter le coworking dans le tableau, en automatisant la génération d'un id. On récupère le dernier élément du tableau et on ajoute +1 à son id.
+    // let coworking = req.body
+
+    const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
+    // let coworking = {id: newId, superficy : req.body.superficy, capacity : req.body.capacity, name: req.body.name}
+
+    // ... SPREAD OPERATOR
+    let coworking = { id: newId, ...req.body }
+    mockCoworkings.push(coworking)
+
+    // On renvoie un objet qui contient les proriétés message et data
+    // message: `Le coworking a bien été ajouté`
+    const result = { message: `Le coworking a bien été ajouté`, data: coworking }
+    res.json(result)
 })
 
-app.checkout('/api/coworkings/:id', (req, res) => {
-  let result = mockCoworkings.find(el => el.id === parseInt(req.params.id))
-
- if(!result){
- result = `Aucun élément ne correspond à l'id ${req.params.id}`
+app.put('/api/coworkings/:id', (req, res) => {
+let coworking = mockCoworkings.find(el => el.id === parseInt(req.params.id))
+let result;
+if (coworking) {
+coworking.superficy = req.body.superficy
+result = { message: `Coworking Modifié`, data: coworking}
+} else {
+    result ={ message : `Le coworking n'existe pas`, data: {}}
 }
-  res.send(result)
-})
-
-
-app.get('/names', (req, res) => {
-    // Une requête ne peut renvoyer qu'une seule et unique réponse
-    // D'abord, on créé une chaîne de caractères à partir des éléments du tableau, puis on la renvoie dans une réponse
-    // => Paul et Pierre et Mathilde !
-    let sentence = ""
-
-    arrUsers.forEach(obj => {
-        sentence += obj.name + " "
-    })
-
-    sentence += "!"
-    res.send(sentence)
-})
-
-app.get('/names/:id', (req, res) => {
-   // console.log(parseInt(req.params.id))
-    // Implémenter le test pour sélectionner dans le tableau l'objet dont l'id correspond à l'id passé en paramètre d'url
-    const urlId = parseInt(req.params.id)
-
-   // for (let i = 0; i < arrUsers.length; i++) { //j'ouvre une boucle for qui me permet de parcourir mon tableau 
-  // const element = arrUsers[i]                   // je déclare ma constante qui contient mon tableau ainsi que l'id
- // if(element.id === parseInt(req.params.id)) { // mon if : le parseint est une fonction qui sert a analyser des strings donc a le mettre en nombre entier ici, donc entre autre je vérifie que l'element est le même que l'ID dans le tableau 
-   // result = arrUsers[i].name // je déclare mon résultat
-   // break;
-  //}
-   // }
-  let result = arrUsers.find(el => el.id === urlId)
-   if (!result) {
-    result = "not found"
-   } else {
-    result = result.name
-   }
-    res.send(result) // j'envoie la réponse 
+res.json(result)
 })
 
 app.listen(port, () => {
